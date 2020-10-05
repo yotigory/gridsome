43f3fb977a436d71a9a12694d1cd7ca965024e3d
yotigory <yotigoo@gmail.com>
2020-10-05 16:31:07 +0900

    コメント追加

--- src/main.js
+++ src/main.js
@@ -13,5 +13,6 @@ import '~/assets/css/style.css'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  //bootstrap
   Vue.use(BootstrapVue)
 }
