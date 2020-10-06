cfec5fcbfddcf22e3823ad475bf0533547fb5782
yotigory <yotigoo@gmail.com>
2020-10-06 18:45:07 +0900

    記事に投稿日と更新日を入れる

--- src/templates/StepPost.vue
+++ src/templates/StepPost.vue
@@ -1,7 +1,11 @@
 <template>
   <Layout>
     <article class="Step">
-    <h1 class="h2 mb-5">{{ $page.stepPost.title }}</h1>
+    <h1 class="h2 mb-3">{{ $page.stepPost.title }}</h1>
+    <div class="border-top border-bottom border-info pt-2 pr-2">
+    <p class="h6 text-right">投稿日：{{ $page.stepPost.createdAt }}</p>
+    <p class="h6 text-right">更新日：{{ $page.stepPost.updatedAt }}</p>
+    </div>
     <VueRemarkContent />
   </article>
   </Layout>
@@ -12,8 +16,8 @@
     stepPost(id: $id) {
       title
       tag
-      createdAt
-      updatedAt
+      createdAt (format: "YYYY年M月D日")
+      updatedAt (format: "YYYY年M月D日")
       content
     }
   }
