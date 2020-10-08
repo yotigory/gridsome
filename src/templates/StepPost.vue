12a31172eab8b035c0364ff599a60996c8d3260d
yotigory <yotigoo@gmail.com>
2020-10-08 15:05:15 +0900

    タイトルの色変更

--- src/templates/StepPost.vue
+++ src/templates/StepPost.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <article class="Step">
-    <h1 class="h2 mb-3">{{ $page.stepPost.title }}</h1>
+    <h1 class="h2 mb-3 text-info">{{ $page.stepPost.title }}</h1>
     <div class="border-top border-bottom border-info pt-2 pr-2">
     <p class="h6 text-right">投稿日：{{ $page.stepPost.createdAt }}</p>
     <p class="h6 text-right">更新日：{{ $page.stepPost.updatedAt }}</p>
